b8472a0f8a66f318eac67fe1ec27fa2e6d7e274f
Juan Ignacio Roldan <juanignacioroldan01@gmail.com>
2020-12-23 02:35:34 -0300

    img reference error from Navbar fixed

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -7,7 +7,9 @@ export const Navbar = () => {
             <NavLink className="navbar-brand" to="/">
                 <span className="d-block d-lg-none">Roldan Catalini</span>
                 <span className="d-block d-lg-none">Juan Ignacio</span>
-                <span className="d-none d-lg-block"><img className="img-fluid img-profile rounded-circle mx-auto mb-2" src="./assets/images/resume-pic.jpg" alt="" /></span>
+                <span className="d-none d-lg-block">
+                    <img className="img-fluid img-profile rounded-circle mx-auto mb-2" src="../assets/images/resume-pic.jpg" alt="Juan Ignacio Roldan" />
+                </span>
             </NavLink>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span className="navbar-toggler-icon"></span></button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
